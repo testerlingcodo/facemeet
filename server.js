77ca83cf0f35cf294e3f929f41408014bd344eaa
testerlingcodo <testerlingcodo@gmail.com>
2026-03-22 02:21:00 +0800

    Fix CORS to allow all origins

--- server.js
+++ server.js
@@ -6,7 +6,10 @@ const path = require('path');
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
-  cors: { origin: '*' }
+  cors: {
+    origin: '*',
+    methods: ['GET', 'POST']
+  }
 });
 
 app.use(express.static(path.join(__dirname, 'public')));
